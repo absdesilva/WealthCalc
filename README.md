8c20d5961c5edab2a5001aa5de05f0fbdf16ebc8
pandamalli <shehan.bs@gmail.com>
2019-08-03 06:38:44 +1000

    Add project files.

---
+++ README.md
@@ -0,0 +1,23 @@
+# WealthCalc
+SIT313 - Developing Client-Server Web Applications | Project 1
+Author - Buddhima Sehan - SID218215945
+
+## Description
+Wealth Calc helps you to be smart about your finance. It is built for everyone to plan easier, be more organized and stay in sync with daily cash flow.
+
+# Features
++ Budget Tracker(Track Expenses/Income)
++ Helps to manage Savings
++ Shows current cash in hand
++ Glance over spendings/earnings on the go
++ Categorizable Expenses/Income
++ Helps to Improve your net income over time.
+
+--------------
+## TIMELINE
+--------------
++ 14/07/2019 - Initial start of WealthCalc 
++ 22/07/2019 - Created project files and updated the GITRepo 
++ 24/07/2019 - Created app icon and updated different resolution folders
++ 25/07/2019 - Added navigation bar
+
---
+++ WealthCalc/MainActivity.cs
@@ -0,0 +1,66 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace WealthCalc
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+
+
+        TextView textCashInHand, textSavings, textIncome, textExpenses;
+        double totalCashInHand, totalSavings, totalIncome, totalExpenses = 0;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+           
+
+            SetContentView(Resource.Layout.activity_main);
+
+            //navigation button event
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.SetOnNavigationItemSelectedListener(this);
+
+            //get home page text view id and assign values
+            textCashInHand = FindViewById<TextView>(Resource.Id.showcashinhand);
+            textSavings = FindViewById<TextView>(Resource.Id.showsavings);
+            textIncome = FindViewById<TextView>(Resource.Id.showincome);
+            textExpenses = FindViewById<TextView>(Resource.Id.showexpenses);
+
+            textCashInHand.Text = "$" + totalCashInHand;
+            textSavings.Text = "$" + totalSavings;
+            textIncome.Text = "$" + totalIncome;
+            textExpenses.Text = "$" + totalExpenses;
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_home:
+                    //textMessage.SetText(Resource.String.title_home);
+                    return true;
+                case Resource.Id.navigation_dashboard:
+                    //textMessage.SetText(Resource.String.title_dashboard);
+                    return true;
+                case Resource.Id.navigation_notifications:
+                    //textMessage.SetText(Resource.String.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    }
+}
+
